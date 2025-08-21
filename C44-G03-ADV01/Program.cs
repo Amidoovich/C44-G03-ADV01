693e2fa3a2135171bcb55c99c0be46073a12199b
ahmed <ahmed@AHMED_SAMY>
2025-08-21 23:44:24 +0300

    Add project files.

---
+++ C44-G03-ADV01/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace C44_G03_ADV01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
