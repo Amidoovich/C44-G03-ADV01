2ea5eff0a9c906f6af4b8d22832929d746b8577f
ahmed <ahmed@AHMED_SAMY>
2025-08-23 15:41:32 +0300

    Q1

--- C44-G03-ADV01/Program.cs
+++ C44-G03-ADV01/Program.cs
@@ -2,9 +2,31 @@
 {
     internal class Program
     {
+        
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Q1
+
+            //Range<int> IntRange = new Range<int>(10, 5);
+            //Console.WriteLine($"6 is in Range => {IntRange.IsInRange(6)}");
+            //Console.WriteLine($"11 is in Range => {IntRange.IsInRange(11)}");
+            //Console.WriteLine($"the Range is {IntRange.length()}");
+
+            //Console.WriteLine();
+
+            //Range<double> DoubleRange = new Range<double>(10.3, 5.4);
+            //Console.WriteLine($"6.5 is in Range => {DoubleRange.IsInRange(6.5)}");
+            //Console.WriteLine($"11.5 is in Range => {DoubleRange.IsInRange(11.5)}");
+            //Console.WriteLine($"the Range is {DoubleRange.length()}");
+
+
+
+            #endregion
+
+            #region Q2
+
+            #endregion
         }
     }
 }
---
+++ C44-G03-ADV01/Range.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C44_G03_ADV01
+{
+    internal class Range<T> where T : INumber<T>
+    {
+        public T Max { get; }
+        public T Min { get; }
+
+        public Range(T max,T min)
+        {
+            Max = max;
+            Min = min;
+
+        }
+
+        public bool IsInRange(T value)
+        { 
+            return value.CompareTo(Min) >= 0 && value.CompareTo(Max) <= 0;
+        }
+
+        public T length()
+        { 
+            return  Max - Min;
+        }
+    }
+}
